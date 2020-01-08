b5fcd8eafd16a6c35539f6cec65412f46064b1c1
rupaku <rupakumari201@yahoo.com>
2020-01-08 17:47:35 +0530

    datetime implementation

---
+++ python basics/datetime.py
@@ -0,0 +1,10 @@
+import datetime
+from time import time
+print(datetime.datetime.now())
+# print(datetime.time())
+# print(datetime.time(5,45,21))
+# print(datetime.date.today())
+# print(datetime.date.today.year())
+# print(datetime.time())
+print(dir(datetime))
+print(datetime.date.date.fromtimestamp(1326244364))
