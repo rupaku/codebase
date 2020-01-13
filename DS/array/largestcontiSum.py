3d2ddab7784c191ef1a9ac5206fb5e8e4dd00092
rupaku <rupakumari201@yahoo.com>
2020-01-13 16:03:50 +0530

    largest contiguous sum

--- DS/array/largestcontiSum.py
+++ DS/array/largestcontiSum.py
@@ -13,4 +13,18 @@ def large_conti_sum(arr):
 
 arr=[1,2,-1,3,4,10,10,-10,-1]
 print(large_conti_sum(arr)) #29
+#
 
+#non negative sum array
+def longestSubarry(arr, n):
+    res = 0
+    for i in range(n):
+        curr_count = 0
+        while (i < n and arr[i] >= 0):
+            curr_count += 1
+            i += 1
+        res = max(res, curr_count)
+    return res
+
+arr=[1, 2, 5, 7, 2, 3]
+print(longestSubarry(arr))
