c85387ca0d90f55cd252cfbc8262e0285e2f58a6
rupaku <rupakumari201@yahoo.com>
2020-04-05 00:07:39 +0530

    array questions leetcode

---
+++ DS/array/HappyNum.py
@@ -0,0 +1,51 @@
+'''
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example:
+
+Input: 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+'''
+
+'''
+To solve this, we will follow these steps −
+
+Here we will use the dynamic programming approach, and solve this using recursion
+Base case is, when n = 1, then return true
+When n is already visited, return false
+mark n as visited
+n := n as string, l := list of all digits in n
+temp := squared sum of all digits
+return function recursively with parameter temp and visited list
+'''
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+
+        return n == 1
+
+
+
+obj = Solution()
+n = 19
+print(obj.isHappy(n))
+
---
+++ DS/array/SingleNum.py
@@ -0,0 +1,31 @@
+'''
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+'''
+
+# As there is each number twice, we can perform XOR to cancel out that element. because we know y XOR y = 0
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = nums[0]
+        for i in range(1, len(nums)):
+            res = res ^ nums[i]
+        return res
+
+
+obj = Solution()
+nums = [2, 2, 1]
+obj.singleNumber(nums)
