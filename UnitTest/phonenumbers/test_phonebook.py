e5eb4e84bb857c9b54f95cd77ce89f5a864c3895
rupaku <rupakumari201@yahoo.com>
2020-05-30 10:37:00 +0530

    test module change

--- UnitTest/phonenumbers/test_phonebook.py
+++ UnitTest/phonenumbers/test_phonebook.py
@@ -3,13 +3,19 @@ import unittest
 
 class PhoneBookTest(unittest.TestCase):
 
+    def setUp(self):
+        self.phonebook =PhoneBook()
+
+    def tearDown(self):
+        pass
+
     def test_lookup_by_name(self):
-        phonebook = PhoneBook()
-        phonebook.add("bob","12345")
-        number= phonebook.lookup("bob")
+        #phonebook = PhoneBook()
+        self.phonebook.add("bob","12345")
+        number= self.phonebook.lookup("bob")
         self.assertEqual("12345",number)
 
     def test_missing_name(self):
-        phonebook = PhoneBook()
+        #phonebook = PhoneBook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
